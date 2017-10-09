669aa73511ec3a11bd9432288399efd243821bed
Justin Ndwiga <ndwigajustin@gmail.com>
2017-10-09 14:56:56 +0300

    Renamed files and variables according to naming conventions

--- max_and_min_values.py
+++ max_and_min_values.py
@@ -1,15 +1,16 @@
-def findMaxMin(list=[]):
+def findMaxMin(a=[]):
     final = []
     #check if list has an index 2 or more
-    if len(list) < 2:
+    if len(a) < 2:
         return "You can only compare 2 or more digits"
     else:
-        list.sort()
+        a.sort()
         #if all numbers equal return the number.[1, 1, 1] should return [1]
-        if list[0] == list[-1]:
-            final.append(list[0])
+        if a[0] == a[-1]:
+            final.append(a[0])
         else:
             #return the  min and max respectively
-            final.append(list[0])
-            final.append(list[-1])
+            final.append(a[0])
+            final.append(a[-1])
         return final
+print (findMaxMin([2,-10,3,4,9,100,1000,50]))
